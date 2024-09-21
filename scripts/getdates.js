f0431c25a55b4f236e264fc1cb3aaea85531fa6d
PriVaAl <priscilla.valverde@gmail.com>
2024-09-21 15:42:05 -0600

    reviews

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,25 +1,7 @@
-// Select the DOM elements for output
-const full = document.querySelector("#full");
-const worldfull = document.querySelector("#world-full");
-const short = document.querySelector("#short");
-const medium = document.querySelector("#medium");
-const year = document.querySelector("#year");
-const month = document.querySelector("#month");
-const day = document.querySelector("#day");
-const dayofweek = document.querySelector("#dayofweek");
-
-// Use the Date object
+const year = document.querySelector("#currentyear");
 const today = new Date();
+year.textContent = `${today.getFullYear()}`;
 
-// Display date formats
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(today)}</span>`;
-worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(today)}</span>`;
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat("en-US", { dateStyle: "short" }).format(today)}</span>`;
-medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(today)}</span>`;
-
-// Display individual date components
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth() + 1}</span>`; // months are 0-based
-day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+const lastMofifiedElement = document.querySelector("#lastModified")
+lastMofifiedElement.textContent = "Last Modification: " + document.lastModified;
 
