2024f672f6b03e44b5e5a567543801bf5c8a0134
PriVaAl <priscilla.valverde@gmail.com>
2024-09-27 16:19:18 -0600

    Updates CSS

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,9 +5,32 @@ year.textContent = `${today.getFullYear()}`;
 const lastMofifiedElement = document.querySelector("#lastModified")
 lastMofifiedElement.textContent = "Last Modification: " + document.lastModified;
 
-const hamburgerButton = document.getElementById('hamburger');
-const nav = document.querySelector('nav');
+const hamburgerButton = document.getElementById('#hamburger');
+const navigation = document.querySelector('.navigation');
 
 hamburgerButton.addEventListener('click', () => {
-    nav.classList.toggle('open');
+    navigation.classList.toggle('open');
+    hamburgerButton.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const headerDynamic = document.getElementById('headerDynamic');
+    const defaultHeaderText = 'Home';
+
+    const savedHeaderText = localStorage.getItem('headerDynamicText');
+
+    if (savedHeaderText) {
+        headerDynamic.textContent = savedHeaderText;
+    }else {
+        headerDynamic.textContent = defaultHeaderText;
+    }
+
+    const hamburgerItems = document.querySelectorAll('.navigation ul li a');
+
+    function handleHamburgerItemClick(event) {
+        event.preventDefault();
+        
+    
+    }
+}
+)
