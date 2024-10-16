2f1af4fc67a3764b6ebf0c04a7589e4d157f5ac7
PriVaAl <priscilla.valverde@gmail.com>
2024-10-15 19:04:33 -0600

    reviews

--- scripts/newdates.js
+++ scripts/newdates.js
@@ -1,6 +1,6 @@
 const year = document.querySelector("#currentyear");
 const today = new Date();
-year.textContent = `${today.getFullYear()}`;
+year.textContent = today.getFullYear();
 
 const lastMofifiedElement = document.querySelector("#lastModified")
 lastMofifiedElement.textContent = "Last Modification: " + document.lastModified;
