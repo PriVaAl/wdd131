897942045a766f32592274066018d6f2d487a4d6
PriVaAl <priscilla.valverde@gmail.com>
2024-10-16 20:43:59 -0600

    Review

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,5 +3,5 @@ const today = new Date();
 year.textContent = `${today.getFullYear()}`;
 
 const lastModifiedElement = document.querySelector("#lastModified")
-lastMofifiedElement.textContent = "Last Modification: " + document.lastModified;
+lastModifiedElement.textContent = "Last Modification: " + document.lastModified;
 
